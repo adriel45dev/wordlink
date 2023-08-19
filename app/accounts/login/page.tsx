ff576a8173e542df171087840fee51132bd72267
adriel45dev <45adrielalves45@gmail.com>
2023-08-19 18:44:17 -0300

    commit test

--- app/accounts/login/page.tsx
+++ app/accounts/login/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Login() {
   return (
     <main className="flex justify-center items-center h-[calc(100vh-100px)] overflow-auto dark:bg-gray-900 dark:text-white">
-      <h1 className="text-6xl">Login...</h1>
+      <h1 className="text-6xl">Login... 2</h1>
     </main>
   );
 }
