5cecce15ead35b4f4cd943fddee92d047008c9df
adriel45dev <45adrielalves45@gmail.com>
2023-10-03 20:10:49 -0300

    Login

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
         </div>
 
         <Link
-          href={"/collection/library"}
+          href={user ? "/collection/library" : "/accounts/signup"}
           className="mb-6 inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
           Start now
