86d61f2c30525c3c32da933d2d9f592f89011c06
adriel45dev <45adrielalves45@gmail.com>
2023-10-04 15:50:06 -0300

    Search

--- app/collection/vocabulary/components/Table.tsx
+++ app/collection/vocabulary/components/Table.tsx
@@ -19,44 +19,12 @@ type TableProps = {
 
 export default function Table({ vocabulary }: TableProps) {
   return (
-    <section className="flex w-full flex-col gap-2 overflow-x-auto rounded-lg  px-16 py-4 shadow-md">
-      <div className="w-full text-sm text-slate-400">
-        Total: {Object.keys(vocabulary).length}
-      </div>
-      {/* Search */}
-      <div className="flex flex-row items-center justify-between ">
-        <label htmlFor="vocabulary-search" className="sr-only">
-          Search
-        </label>
-        <div className="relative w-full">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <LupaIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-          </div>
-          <input
-            type="text"
-            id="vocabulary-search"
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="Search for vocabulary"
-          />
-        </div>
-      </div>
+    <div className="flex w-full flex-col gap-2 overflow-x-auto rounded-lg  px-16 py-4 shadow-md">
       <div className="flex w-full flex-row items-center justify-between rounded-lg bg-slate-800 px-4 py-2 text-white">
         <button>[ type ^]</button>
         <button>[ filter ^]</button>
       </div>
       <ul className="flex w-full flex-col items-center justify-center gap-2 rounded-lg bg-slate-800 p-2 text-white">
-        {/* <li className="flex w-full flex-row items-center gap-4 rounded-lg px-2 py-2 odd:bg-slate-900 even:bg-slate-700 hover:bg-blue-500">
-          <div className="flex h-8 w-8 items-center justify-center rounded-full border border-slate-300 bg-yellow-400/10">
-            1
-          </div>
-          <div className="flex w-full flex-col justify-center">
-            <div className="text-lg font-bold">word</div>
-            <div className="flex flex-wrap text-sm text-slate-300">
-              Lorem ipsum dolor; drop;
-            </div>
-          </div>
-        </li> */}
-
         {Object.keys(vocabulary)
           .filter((e) => vocabulary[e].state != WordLinkType.IGNORE)
           .map((word, key) => {
@@ -79,6 +47,6 @@ export default function Table({ vocabulary }: TableProps) {
             );
           })}
       </ul>
-    </section>
+    </div>
   );
 }
--- app/collection/vocabulary/page.tsx
+++ app/collection/vocabulary/page.tsx
@@ -5,6 +5,7 @@ import Table from "./components/Table";
 import NoUserLogged from "@/app/components/NoUserLogged";
 import { UserContext } from "@/app/context/UserContext";
 import { UserLogginContext } from "@/app/context/UserLoggingContext";
+import LupaIcon from "@/public/icons/LupaIcon";
 
 export default function Vocabulary() {
   const { setTab } = useContext(NavbarContext);
@@ -13,12 +14,14 @@ export default function Vocabulary() {
   }>({});
   const { user } = useContext(UserContext);
   const { isUser } = useContext(UserLogginContext);
+  const [search, setSearch] = useState("");
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => setTab("vocabulary"), []);
 
   useEffect(() => {
     if (!user || !isUser) return;
+    if (search) return;
     const { language_key, username } = user;
     const dataJSON = localStorage.getItem(
       `${language_key}_${username}_vocabulary`,
@@ -30,12 +33,64 @@ export default function Vocabulary() {
       setVocabulary({});
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+  }, [user, search]);
+
+  const filterSearch = (value: string) => {
+    const filtered = Object.keys(vocabulary).filter((key) => {
+      return key.includes(value.toLowerCase());
+    });
+
+    let sorted = filtered.sort((a, b) => {
+      return a.indexOf(value) - b.indexOf(value);
+    });
+
+    const result: { [key: string]: { des: string[]; state: string } } = {};
+
+    sorted.forEach((s) => {
+      result[s] = {
+        ...vocabulary[s],
+      };
+    });
+
+    setVocabulary(result);
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    filterSearch(value);
+    setSearch(value);
+  };
+
+  console.log(search);
 
   return (
     <>
       <h1 className="my-6 text-4xl font-bold text-white">Vocabulary</h1>
       <div className="flex w-full flex-1 flex-col items-center ">
+        <div className="flex w-full flex-col gap-2 overflow-x-auto rounded-lg  px-16 py-4">
+          <div className="w-full text-sm text-slate-400">
+            Total: {Object.keys(vocabulary).length}
+          </div>
+          {/* Search */}
+          <div className="flex flex-row items-center justify-between ">
+            <label htmlFor="vocabulary-search" className="sr-only">
+              Search
+            </label>
+            <div className="relative w-full">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <LupaIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+              </div>
+              <input
+                type="text"
+                id="vocabulary-search"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                placeholder="Search for vocabulary"
+                onChange={handleSearch}
+                value={search}
+              />
+            </div>
+          </div>
+        </div>
         <Table vocabulary={vocabulary} />
       </div>
     </>
